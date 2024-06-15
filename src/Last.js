f5ef4fcb798f9805d5b4bdc45f28b0cef13f8bd8
Chidera Benedict <chiderabenedict04@gmail.com>
2024-06-15 01:50:06 -0700

    first commit

--- src/Last.js
+++ src/Last.js
@@ -6,16 +6,23 @@ function LastNav(){
         <div className="main_container">
             <div className="ist_container">
                 <section className='details'>
-                    <Link to=""></Link>
+                    <Link to="/errorpage">Amazon.com</Link>
+                    <Link to="/errorpage">Your Lists</Link>
+                    <Link to="/errorpage">Find a Gift</Link>
+                    <Link to="/errorpage">Browsing History</Link>
+                    <Link to="/errorpage">Returns</Link>
                 </section>
                 <section className='details'>
-
+                <Link to="/errorpage">Your Orders</Link>
+                <Link to="/errorpage">Gift Cards</Link>
+                <Link to="/errorpage">Your Account</Link>
+                <Link to="/errorpage">Sell Products on Amazon</Link>
+                <Link to="/errorpage">Recalls and Product Safety Alerts</Link>
+                <Link to="/errorpage">Customer Service</Link>
                 </section>
             </div>
 
-            <div className='second_container'>
-
-            </div>
+           <div className='writter'>Cloned by @Okafor Benedict </div>
         </div>
     )
 }
